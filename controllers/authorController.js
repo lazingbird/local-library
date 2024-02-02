3de519ea63fe8bb9d21466aa69051d644e0a16e1
lazingbird <lazingbird@protonmail.com>
2024-02-02 17:46:43 -0300

    fix bug that made impossible to authors name to have spaces

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -43,16 +43,12 @@ exports.author_create_post = [
     .trim()
     .isLength({ min: 1 })
     .escape()
-    .withMessage("First name must be specified.")
-    .isAlphanumeric()
-    .withMessage("First name has non-alphanumeric characters."),
+    .withMessage("First name must be specified."),
   body("family_name")
     .trim()
     .isLength({ min: 1 })
     .escape()
-    .withMessage("Family name must be specified.")
-    .isAlphanumeric()
-    .withMessage("Family name has non-alphanumeric characters."),
+    .withMessage("Family name must be specified."),
   body("date_of_birth", "Invalid date of birth")
     .optional({ values: "falsy" })
     .isISO8601()
