b8e0903134a85a3af321019438d02562e2b52160
lazingbird <lazingbird@protonmail.com>
2024-02-02 17:15:22 -0300

    prepare to deployment

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -6,14 +8,28 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+const mongoDB = process.env.MONGODB_URI;
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = main().catch((err) => console.log(err));
+
+main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
@@ -26,6 +42,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -36,6 +54,14 @@ app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
 // error handler
 app.use(function (err, req, res, next) {
